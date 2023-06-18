365986f78d0122c7fcb8818809a34ea2cb3dd815
Jesus Monge <jesus_jaml@outlook.com>
2023-06-18 01:39:44 -0500

    [FEAT] controlled search input form + display list of movies inside a responsive grid layout + fetch movies using input query and API

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,69 @@ import { useState } from 'react'
 import './App.css'
 import Movies from './componentes/Movies'
 import { useMovies } from './hooks/useMovies'
+import { useEffect } from 'react'
+import { useRef } from 'react'
+
+function useSearch() {
+  const [searchText, setSearchText] = useState('')
+  const [error, setError] = useState()
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = searchText === ''
+      return
+    }
+
+    if (searchText === '') {
+      setError('Please enter a movie name')
+      return
+    }
+
+    if (searchText.length < 3) {
+      setError('Please enter at least 3 characters')
+      return
+    }
+
+    setError(null)
+  }, [searchText])
+
+  return { searchText, setSearchText, error }
+}
 
 function App() {
-  const [searchText, setSearchText] = useState('')
-  const { movies, error } = useMovies(searchText)
+  const { searchText, setSearchText, error } = useSearch()
+  const { movies, error: errorReposinse } = useMovies(searchText)
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target)
-    const { search } = Object.fromEntries(formData)
-    setSearchText(search)
+  const handleChange = (e) => {
+    const { value } = e.target
+
+    if (value === ' ') return
+
+    setSearchText(value)
   }
 
   return (
     <div className="container">
       <header>
         <h1>Movie Search App</h1>
-        <form className="form" onSubmit={handleSubmit}>
+        <form className="form">
           <input
             placeholder="Forest gump, Fast and furious, Avengers ..."
             type="search"
             autoComplete="off"
             name="search"
+            value={searchText}
+            onChange={handleChange}
+            style={{ border: error && '1px solid red' }}
           />
           <button>Search</button>
         </form>
+        {error && <p className="error">{error}</p>}
       </header>
 
       <main>
-        <Movies movies={movies} error={error} />
+        <Movies movies={movies} error={errorReposinse} />
       </main>
     </div>
   )
--- src/componentes/ListOfMovies.jsx
+++ src/componentes/ListOfMovies.jsx
@@ -1,9 +1,9 @@
 export function ListOfMovies({ movies }) {
   return (
-    <ul>
+    <ul className="movies">
       {movies.map(({ id, title, poster, year }) => {
         return (
-          <li key={id}>
+          <li className="movie" key={id}>
             <h3>{title}</h3>
             <p>{year}</p>
             <img src={poster} alt={title} />
